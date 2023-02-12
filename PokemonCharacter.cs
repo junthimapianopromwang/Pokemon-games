32489071231f7a5a7fc759a836dca1077c694481
junthimapianopromwang <junthima.p@kkumail.com>
2023-02-12 23:59:27 +0700

    Add project files.

---
+++ PokemonCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_games
+{
+    public class PokemonCharacter
+    {
+        public string Name;
+        public int HP;
+        public int Power;
+        public Bitmap Image;
+    }
+}
---
+++ Venusaur.cs
@@ -0,0 +1,20 @@
+﻿using Pokemon_games.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_games {
+        public class DrowVenusaurzee : PokemonCharacter
+        {
+            public DrowVenusaurzee()
+            {
+                this.Name = "Drowzee";
+                this.HP = 500;
+                this.Power = 15;
+                this.Image = Resources.หนอน3;
+            }
+        }
+}
+
